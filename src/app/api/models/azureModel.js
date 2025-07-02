08f4d1687afd4dd38f45e4dc3293f2cd787311b6
muhammadibad@0338 <mibad0338@gmail.com>
2025-07-02 14:24:09 +0200

    azure schema setup

--- src/app/api/models/azureModel.js
+++ src/app/api/models/azureModel.js
@@ -1,51 +1,40 @@
 import mongoose, { Schema } from "mongoose";
 
-const azureSchema = new Schema(
-    {
-        title: String,
-        description: String,
-    },
-    {
-        timestamps: true,
+const SavingsPlanSchema = new mongoose.Schema({
+    unitPrice: Number,
+    retailPrice: Number,
+    term: String,
+}, { _id: true });
+
+const azureSchema = new mongoose.Schema({
+    currencyCode: String,
+    tierMinimumUnits: Number,
+    retailPrice: Number,
+    unitPrice: Number,
+    armRegionName: String,
+    location: String,
+    effectiveStartDate: Date,
+    meterId: String,
+    meterName: String,
+    productId: String,
+    skuId: String,
+    productName: String,
+    skuName: String,
+    serviceName: String,
+    serviceId: String,
+    serviceFamily: String,
+    unitOfMeasure: String,
+    type: String,
+    isPrimaryMeterRegion: Boolean,
+    armSkuName: String,
+    ssavingsPlan: {
+        type: [SavingsPlanSchema],
+        required: false, // optional — you can also remove this line
+        default: undefined // optional: don't include field if it's not provided
     }
-);
+}, { timestamps: true });
+
 
 const Azure = mongoose.models.Azure || mongoose.model("Azure", azureSchema);
 
 export default Azure;
-
-
-
-// import mongoose from 'mongoose';
-
-// const SavingsPlanSchema = new mongoose.Schema({
-//   unitPrice: Number,
-//   retailPrice: Number,
-//   term: String,
-// }, { _id: true });
-
-// const AzurePricingSchema = new mongoose.Schema({
-//   currencyCode: String,
-//   tierMinimumUnits: Number,
-//   retailPrice: Number,
-//   unitPrice: Number,
-//   armRegionName: String,
-//   location: String,
-//   effectiveStartDate: Date,
-//   meterId: String,
-//   meterName: String,
-//   productId: String,
-//   skuId: String,
-//   productName: String,
-//   skuName: String,
-//   serviceName: String,
-//   serviceId: String,
-//   serviceFamily: String,
-//   unitOfMeasure: String,
-//   type: String,
-//   isPrimaryMeterRegion: Boolean,
-//   armSkuName: String,
-//   savingsPlan: [SavingsPlanSchema]
-// }, { timestamps: true });
-
-// export default mongoose.models.AzurePricing || mongoose.model('AzurePricing', AzurePricingSchema);
