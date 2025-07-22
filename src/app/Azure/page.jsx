83ad403f11a8ca81986c267dec12e40074fe4985
muhammadibad@0338 <mibad0338@gmail.com>
2025-07-22 17:11:11 +0200

    working on Azure data rendring

--- src/app/Azure/page.jsx
+++ src/app/Azure/page.jsx
@@ -3,13 +3,31 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { getAzureData } from '@/lib/actions/azureActions';
+import { Box, colors, Container, Grid } from '@mui/material';
+import { makeStyles } from "@mui/styles";
+import { styled } from '@mui/system';
 
 
+
+
+const MainCntnr = styled(Grid)(({ theme }) => ({
+    padding: '24px 24px 24px 24px',
+    borderRadius: '30px',
+    border: '1px solid rgba(255, 255, 255, 0.20)',
+    background: 'rgba(255, 255, 255, 0.10)',
+    boxShadow: '0px 4px 12px 0px rgba(0, 0, 0, 0.25)',
+    backdropFilter: 'blur(10px)',
+}));
 
 
 
 export default function AzurePage() {
     const dispatch = useDispatch();
+
+
+
+
+
     const { azure, loading, error } = useSelector((state) => state.azure);
     const [paramData, setParamData] = useState({
         'page': 2,
@@ -21,13 +39,18 @@ export default function AzurePage() {
     }, [dispatch, paramData]);
 
 
-    if (loading) return <div>Loading...</div>;
+    if (loading || Object.keys(azure).length == 0) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
+
+
     return (
-        <div>
-            <h2>Azure Pricing</h2>
-            <pre>{JSON.stringify(azure, null, 2)}</pre>
-        </div>
+        <MainCntnr container >
+
+            <Grid size={{xs:12}} >
+                <h2>Azure Pricing</h2>
+                <pre>{JSON.stringify(azure, null, 2)}</pre>
+            </Grid>
+        </MainCntnr>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,44 @@
+'use client';
+import { useEffect, useState } from 'react';
 import Image from "next/image";
 import AzurePage from "./Azure/page";
 
 
 
+import { Box, colors, Container, Grid } from '@mui/material';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
+import { styled } from '@mui/system';
 
 
+const WarningSnackbar = styled(Snackbar)(({ theme }) => ({
+  color: '#fff'
+}));
 
-
-
-
-
-
-
-// dispatch(getAzureData({
-//   page: 1,
-//   limit: 25,
-//   armRegionName: "westus,eastus",
-// }));
 
 export default function Home() {
+
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(true)
+
+
   return (
-    <>
-      <AzurePage />
-    </>
+    <Container maxWidth='fl'  >
+      <Grid container >
+        <Grid size={{ xs: 12 }}   >
+          <Box  >
+            <WarningSnackbar
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              open={isSnackbarOpen}
+              autoHideDuration={8000}
+              onClose={() => setIsSnackbarOpen(false)}
+              message="This pricing list is for informational purposes only. 
+                            All prices are subject to the additional terms outlined on the official pricing and service pages"
+            />
+          </Box>
+        </Grid>
+        <Grid size={{ xs: 12 }}   >
+          <AzurePage />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
