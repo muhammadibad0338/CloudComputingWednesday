0479258299394ce2d8c9033d5b7c11d973d99d5e
muhammadibad@0338 <mibad0338@gmail.com>
2025-07-22 13:15:06 +0200

    material UI setip

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -1,6 +1,47 @@
 'use client';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#FFFFFF',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        headTypography: {
+            main: '#0A1929'
+        },
+        ticketBox: {
+            main: '#EBECF0'
+        }
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            ms: 479,
+            sm: 600,
+            nineS: 900,
+            md: 1090,
+            lg: 1200,
+            fl: 1350,
+            xl: 1556,
+        },
+    }
+});
+
+
+
 export default function ClientLayout({ children }) {
     // You can now use hooks, state, effects, etc. here
-    return <>{children}</>;
+    return <>
+        <ThemeProvider theme={theme} >
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    </>;
 }
